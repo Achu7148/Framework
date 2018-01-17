399f001b4919ae03c6558774a89b1d42f530632a
Ashwanth <achu7148@gmail.com>
2018-01-17 20:32:37 +0530

    created login for CRM

--- src/main/java/Ashwanth/Framework/chromebrowserlaunch.java
+++ src/main/java/Ashwanth/Framework/chromebrowserlaunch.java
@@ -1,5 +1,7 @@
 package Ashwanth.Framework;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,24 +10,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class chromebrowserlaunch {
 	 public static void main(String[] args) throws InterruptedException  {
 		 System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe"); //Setting up chrome driver
+		 
 	      WebDriver driver = new ChromeDriver();
 		 
-		 driver.get("http://prince2.sensiple.com/prince-web/ems/login/view");//Launch Chrome Browser
+		 driver.get("https://sensiple-dev.crm.dynamics.com/");//Launch Chrome Browser
 		 
 		 driver.manage().window().maximize();//Maximize the window
 		 
 		 //Enter Username, Password and login
-		 driver.findElement(By.id("j_username")).sendKeys("2305");
-	     driver.findElement(By.id("j_password")).sendKeys("achu7148");
-		 driver.findElement(By.xpath(".//*[@id='loginForm']/button")).click();
-		 driver.findElement(By.id("swipeDetailsId")).click();
-		 Thread.sleep(3000);
-		 driver.findElement(By.id("get-resource-assignment")).click();
-		
-		 Thread.sleep(6000);
+		 driver.findElement(By.id("i0116")).sendKeys("bde1@sensiple4.onmicrosoft.com");
 		 
+		 driver.findElement(By.id("idSIButton9")).click();
 		 
+		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		 
-		 driver.close();
+	     driver.findElement(By.id("i0118")).sendKeys("Sensiple@2017");
+	     
+	     driver.manage().timeouts().implicitlyWait(05, TimeUnit.SECONDS);
+	     
+		 driver.findElement(By.xpath("//input[@value='Sign in']")).click();
+		 
+	     driver.manage().timeouts().implicitlyWait(05, TimeUnit.SECONDS);
+ 
+		 driver.findElement(By.id("idBtn_Back")).click();
+		  
+		 driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		 
+		 driver.quit();
+		 
 	 }
 }
