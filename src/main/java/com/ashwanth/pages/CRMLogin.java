bb1d11fef980d8522d1f80b545561c7ef2ec150f
Ashwanth <achu7148@gmail.com>
2018-01-18 11:19:57 +0530

    Added code
    Code for logging in into CRM

---
+++ src/main/java/com/ashwanth/pages/CRMLogin.java
@@ -0,0 +1,81 @@
+package com.ashwanth.pages;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CRMLogin {
+	
+	private static WebElement element = null;
+	
+	//Username textbox
+	public static WebElement txtbx_UserName(WebDriver driver){
+		 
+        element = driver.findElement(By.id("i0116"));
+
+        return element;
+
+        }
+	
+	//Next button After entering username
+	 public static WebElement btn_Next(WebDriver driver){
+		 
+         element = driver.findElement(By.id("idSIButton9"));
+ 
+         return element;
+ 
+         }
+	 
+	 //password text box
+	 public static WebElement txtbx_Password(WebDriver driver){
+		 
+         element = driver.findElement(By.id("i0118"));
+ 
+         return element;
+ 
+         }
+	 
+	 //Login button
+	 public static WebElement btn_LogIn(WebDriver driver){
+		 
+         element = driver.findElement(By.xpath("//input[@value='Sign in']"));
+ 
+         return element;
+ 
+         }
+	 
+    //Stay signed in No button
+     public static WebElement btn_No(WebDriver driver){
+		 
+         element = driver.findElement(By.id("idBtn_Back"));
+ 
+         return element;
+ 
+         }
+     
+     //wait for element present
+public static WebElement WaitForElement(WebDriver driver){
+		 
+	WebDriverWait wait = new WebDriverWait(driver, 30);
+	
+	 return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("navTabButtonHelpImageIdWeb")));
+ 
+         }
+
+//close the browser
+public static void CloseDriver(WebDriver driver){
+	 
+    driver.quit();
+
+    }
+
+//Wait
+public static void Wait(WebDriver driver){
+	 
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+}
---
+++ src/main/java/com/ashwanth/tests/Test_1_CRMLogin.java
@@ -0,0 +1,79 @@
+package com.ashwanth.tests;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.ashwanth.pages.CRMLogin;
+
+
+public class Test_1_CRMLogin {
+	
+	WebDriver driver;
+	
+	
+	@BeforeMethod
+	public void LaunchBrowser() {
+		
+		 System.setProperty("webdriver.chrome.driver", "C:\\workspace\\qvsbatch1\\SelDrivers\\chromedriver.exe"); //Setting up chrome driver
+		 driver = new ChromeDriver();  
+		 
+	     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+	     driver.get("https://sensiple-dev.crm.dynamics.com/");
+	     
+	     driver.manage().window().maximize();	 
+	}
+    @Test
+	public void Login() {
+		   
+	     CRMLogin.txtbx_UserName(driver).sendKeys("bde1@sensiple4.onmicrosoft.com");
+	     
+	     CRMLogin.btn_Next(driver).click();
+	     
+	     CRMLogin.Wait(driver);
+	 
+	     CRMLogin.txtbx_Password(driver).sendKeys("Sensiple@2017");
+	     
+	     CRMLogin.Wait(driver);
+	     
+	     CRMLogin.btn_LogIn(driver).click();
+	     
+	     CRMLogin.Wait(driver);
+	 
+	     CRMLogin.btn_No(driver).click();
+	     
+	     CRMLogin.WaitForElement(driver);
+
+	     CRMLogin.CloseDriver(driver);
+	 
+	     }
+    
+    @Test
+    public void Login1() {
+		   
+	     CRMLogin.txtbx_UserName(driver).sendKeys("bde1@sensiple4.onmicrosoft.com");
+	     
+	     CRMLogin.btn_Next(driver).click();
+	     
+	     CRMLogin.Wait(driver);
+	 
+	     CRMLogin.txtbx_Password(driver).sendKeys("Sensiple@2017");
+	     
+	     CRMLogin.Wait(driver);
+	     
+	     CRMLogin.btn_LogIn(driver).click();
+	     
+	     CRMLogin.Wait(driver);
+	 
+	     CRMLogin.btn_No(driver).click();
+	     
+	     CRMLogin.WaitForElement(driver);
+
+	     CRMLogin.CloseDriver(driver);
+	 
+	     }
+    
+}
