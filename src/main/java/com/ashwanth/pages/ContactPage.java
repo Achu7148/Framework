8d42cda22049d6cec294b1ccabf6916ecce87c29
Ashwanth <achu7148@gmail.com>
2018-01-18 14:29:17 +0530

    account form filling
    methods for 2 fields has been created

--- src/main/java/com/ashwanth/pages/ContactPage.java
+++ src/main/java/com/ashwanth/pages/ContactPage.java
@@ -1,5 +1,27 @@
 package com.ashwanth.pages;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+
 public class ContactPage {
 
+	private static WebElement element = null;
+	
+	public static WebElement txtbx_AccName(WebDriver driver){
+		 
+        element = driver.findElement(By.id("name_i"));
+
+        return element;
+
+        }
+	
+	public static WebElement txtbx_CntryName(WebDriver driver){
+		 
+        Select element = new Select(driver.findElement(By.id("new_country_i")));
+
+        element.selectByVisibleText("Albania");
+		
+        return null;
+
+        }
 }
