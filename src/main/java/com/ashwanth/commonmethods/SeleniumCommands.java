59f2dfad203f5477c5297753225de486d59b6347
Ashwanth <achu7148@gmail.com>
2018-01-23 19:53:12 +0530

    added browser common method
    still need to edit more in this section

--- src/main/java/com/ashwanth/commonmethods/SeleniumCommands.java
+++ src/main/java/com/ashwanth/commonmethods/SeleniumCommands.java
@@ -1,7 +1,27 @@
 package com.ashwanth.commonmethods;
 
-public class SeleniumCommands {
+import org.openqa.selenium.WebDriver;
 
-	
-	public void 
+public static WebDriver OpenApp(String BrowserName, String url){
+fn_LaunchBrowser(BrowserName);
+fn_OpenURL(url);
+return driver;
+}
+public static void fn_OpenURL(String url){
+driver.get(url);
+driver.manage().window().maximize();
+}
+ 
+public static WebDriver fn_LaunchBrowser(String browsername){
+if(browsername=="CH"){
+System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
+driver= new ChromeDriver();
+}else if(browsername=="FF"){
+driver= new FirefoxDriver();
+}else if(browsername=="IE"){
+System.setProperty("webdriver.ie.driver", "Drivers\\IEDriverServer.exe");
+driver= new InternetExplorerDriver();
+}
+driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+return driver;
 }
