ae0ed90cdfd5369639c530f52c59a4218d784b55
Ashwanth <achu7148@gmail.com>
2018-01-30 15:06:25 +0530

    Update
    Added code for selecting dropdown in account details filling page

--- src/main/java/com/ashwanth/pages/ContactPage.java
+++ src/main/java/com/ashwanth/pages/ContactPage.java
@@ -1,13 +1,14 @@
 package com.ashwanth.pages;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.*;
 
 public class ContactPage {
 
 	private static WebElement element = null;
 	
-	
+	//Account Name Text Box
 	public static WebElement txbx_AccName(WebDriver driver){
 		 
         element = driver.findElement(By.id("name_i"));
@@ -16,15 +17,36 @@ public class ContactPage {
 
         }
 	
-	public static WebElement txtbx_CntryName(WebDriver driver){
+	//Country Name Drop down
+	public static WebElement txtbx_CntryName(WebDriver driver) throws InterruptedException{
 		 
-        Select element = new Select(driver.findElement(By.id("new_country_i")));
+		boolean countryDropdown = driver.findElement(By.xpath("//*[@id='Country_label']")).isEnabled();
+		if (countryDropdown == true) {
+			WebElement element = driver.findElement(By.xpath("//*[@id='Country_label']"));
 
-        element.selectByVisibleText("Albania");
-		
-        return null;
+			Actions action = new Actions(driver);
 
-        }
+			action.moveToElement(element).build().perform();
+			action.click();
+
+			driver.findElement(By.xpath("//*[@id='Country_label']")).click();
+			Thread.sleep(1000);
+			System.out.println("Step-1");
+
+			// Actions action = new Actions(driver);
+			// WebElement element =
+			// driver.findElement(By.xpath("//select[@id='new_country_i']"));
+			// action.doubleClick(element).perform();
+
+			Select element1 = new Select(driver.findElement(By.xpath("//select[@id='new_country_i']")));
+			element1.selectByVisibleText("Albania");
+			Thread.sleep(2000);
+		} else 
+		{
+			System.out.println("object is not present");
+		}
+		return element;
+	}
 	
  public static WebElement txbx_State(WebDriver driver){
 		 
--- src/main/java/com/ashwanth/tests/Test_2_CRMLogin.java
+++ src/main/java/com/ashwanth/tests/Test_2_CRMLogin.java
@@ -1,10 +1,14 @@
 package com.ashwanth.tests;
 
-
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
@@ -17,71 +21,74 @@ import com.ashwanth.pages.FormSelection;
 import com.ashwanth.pages.ContactPage;
 
 public class Test_2_CRMLogin {
-	
+
 	WebDriver driver;
-	
-	
+
 	@BeforeMethod
 	public void LaunchBrowser() {
-	     
-		 System.setProperty("webdriver.chrome.driver", "C:\\workspace\\qvsbatch1\\SelDrivers\\chromedriver.exe"); //Setting up chrome driver
-		 driver = new ChromeDriver();  
-		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-	    
-	     driver.get("https://sensiple-dev.crm.dynamics.com/");
-	     
-	     driver.manage().window().maximize(); 	 
+		System.setProperty("webdriver.chrome.driver", "C:\\workspace\\qvsbatch1\\SelDrivers\\chromedriver.exe"); // Setting
+//																													// up
+//																													// chrome
+//																													// driver
+		driver = new ChromeDriver();
+		//System.setProperty("webdriver.gecko.driver", "C:\\workspace\\Framework\\SeleniumDrivers\\FirefoxDriver.exe");
+		//WebDriver driver = new FirefoxDriver();
+        //driver = new FirefoxDriver();
+		
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.get("https://sensiple-dev.crm.dynamics.com/");
+
+		driver.manage().window().maximize();
 	}
-	
-	
-   @Test
-    public void Login()throws NullPointerException {
-	   
-	     CRMLogin.txtbx_UserName(driver).sendKeys("bde1@sensiple4.onmicrosoft.com");
-	     
-	     CRMLogin.btn_Next(driver).click();
-	     
-	     CRMLogin.Wait(driver);
-	 
-	     CRMLogin.txtbx_Password(driver).sendKeys("Sensiple@2017");
-	     
-	     CRMLogin.Wait(driver);
-	     
-	     CRMLogin.btn_LogIn(driver).click();
-	     
-	     CRMLogin.Wait(driver);
-	 
-	     CRMLogin.btn_No(driver).click();
-	     
-	     CRMLogin.WaitForElement(driver);
-	        
-	     } 
-   
-    
-   @AfterTest
-    public void Account_Page()
-    {
-    	
-	     FormSelection.sales_selection(driver).click();
-   	 
-	     FormSelection.account_selection(driver).click();
-	     
-	     CRMLogin.WaitForElement1(driver);
-	       
-	     FormSelection.click_new(driver).click();
-	     
-	     driver.switchTo().frame("contentIFrame1");
-	     
-	     CRMLogin.WaitForElement2(driver);
-	     
-	     ContactPage.txbx_AccName(driver).sendKeys("Ashwanth");
-	     
-	    // ContactPage.txtbx_CntryName(driver).click();
-	     
-	     
-	     ContactPage.txbx_State(driver).isSelected();
-	       
-	     ContactPage.txbx_State(driver).sendKeys("Tamilnadu");
-    }
+
+	@Test
+	public void Login() throws NullPointerException {
+
+		CRMLogin.txtbx_UserName(driver).sendKeys("bde1@sensiple4.onmicrosoft.com");
+
+		CRMLogin.btn_Next(driver).click();
+
+		CRMLogin.Wait(driver);
+
+		CRMLogin.txtbx_Password(driver).sendKeys("Sensiple@2017");
+
+		CRMLogin.Wait(driver);
+
+		CRMLogin.btn_LogIn(driver).click();
+
+		CRMLogin.Wait(driver);
+
+		CRMLogin.btn_No(driver).click();
+
+		CRMLogin.WaitForElement(driver);
+
+	}
+
+	@AfterTest
+	public void Account_Page() throws InterruptedException {
+
+		FormSelection.sales_selection(driver).click();
+
+		FormSelection.account_selection(driver).click();
+
+		CRMLogin.WaitForElement1(driver);
+
+		FormSelection.click_new(driver).click();
+
+		driver.switchTo().frame("contentIFrame1");
+
+		CRMLogin.WaitForElement2(driver);
+		
+		ContactPage.txbx_AccName(driver).sendKeys("Ashwanth");
+		
+		ContactPage.txtbx_CntryName(driver);
+		
+		
+		
+		
+		
+	}
 }
